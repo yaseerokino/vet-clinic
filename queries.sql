9fe89ec3d263f5e7819f3dd663ac1623634b7d6b
Yaseer Anaisi Okino <37089367+yaseerokino@users.noreply.github.com>
2021-11-08 15:23:06 +0100

    Add queries to query the table based o certain conditions

--- queries.sql
+++ queries.sql
@@ -1,3 +1,11 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * FROM animals WHERE NAME LIKE '%mon';
+SELECT NAME FROM animals WHERE DATE_OF_BIRTH BETWEEN '2016-01-01' AND '2019-12-31';
+SELECT NAME FROM animals WHERE NEUTERED = true AND ESCAPE_ATTEMPTS < 3;
+SELECT DATE_OF_BIRTH from animals WHERE NAME = 'Agumon' OR NAME = 'Pikachu';
+SELECT NAME, ESCAPE_ATTEMPTS FROM animals WHERE WEIGHT_KG > 10.5;
+SELECT * FROM animals WHERE NOT NEUTERED;
+SELECT * FROM animals WHERE NAME != 'Gabumon';
+SELECT * FROM animals WHERE WEIGHT_KG >= 10.4 AND WEIGHT_KG <= 17.3; 
+-- SELECT * FROM animals WHERE WEIGHT_KG >= 10.4 AND WEIGHT_KG <= 17.3 is quivalent to SELECT * FROM animals WHERE WEIGHT_KG  BETWEEN  10.4 AND 17.3
