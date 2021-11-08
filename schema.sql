b16974b165d0237acb3cef131f79c76b8e147e16
Yaseer Anaisi Okino <37089367+yaseerokino@users.noreply.github.com>
2021-11-08 15:21:36 +0100

    Add new table schema

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+    ID INT GENERATED ALWAYS AS IDENTITY,
+    NAME VARCHAR(500),
+    DATE_OF_BIRTH DATE,
+    ESCAPE_ATTEMPTS INT,
+    NEUTERED BOOLEAN,
+    WEIGHT_KG FLOAT
 );
