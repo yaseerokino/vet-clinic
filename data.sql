e2607ba5f3c3da942d2c4e2356ff187478c0b85c
Yaseer Anaisi Okino <37089367+yaseerokino@users.noreply.github.com>
2021-11-13 14:53:41 +0100

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,5 +1,3 @@
-/* Populate database with sample data. */
-
 INSERT INTO animals (NAME, DATE_OF_BIRTH, WEIGHT_KG, NEUTERED, ESCAPE_ATTEMPTS) VALUES ('Agumon', '2020-02-03', 10.23, true, 0);
 INSERT INTO animals (NAME, DATE_OF_BIRTH, WEIGHT_KG, NEUTERED, ESCAPE_ATTEMPTS) VALUES ('Gabumon', '2018-11-15', 8, true, 2);
 INSERT INTO animals (NAME, DATE_OF_BIRTH, WEIGHT_KG, NEUTERED, ESCAPE_ATTEMPTS) VALUES ('Pikachu', '2021-01-07', 15.04, false, 1);
@@ -10,4 +8,17 @@ INSERT INTO animals (NAME, DATE_OF_BIRTH, WEIGHT_KG, NEUTERED, ESCAPE_ATTEMPTS) 
 INSERT INTO animals (NAME, DATE_OF_BIRTH, WEIGHT_KG, NEUTERED, ESCAPE_ATTEMPTS) VALUES ('Angemon', '2005-06-12', -45, false, 1);
 INSERT INTO animals (NAME, DATE_OF_BIRTH, WEIGHT_KG, NEUTERED, ESCAPE_ATTEMPTS) VALUES ('Boarmon', '2005-06-07', 20.4, true, 7);
 INSERT INTO animals (NAME, DATE_OF_BIRTH, WEIGHT_KG, NEUTERED, ESCAPE_ATTEMPTS) VALUES ('Blossom', '1998-10-13', 17, true, 3);
+
+
+INSERT INTO owners (FULL_NAME,AGE) VALUES ('Sam Smith', 34), ('Jennifer Orwell', 19), ('Bob', 45), ('Melody', 77), ('Dean Winchester', 14), ('Jodie Whittaker', 38);
+INSERT INTO species (NAME) VALUES ('Pokemon'), ('Digimon');
+
+UPDATE animals SET species_id=1;
+UPDATE animals SET species_id=2 WHERE NAME LIKE '%mon';
+
+UPDATE animals SET owner_id=1 WHERE NAME='Agumon';
+UPDATE animals SET owner_id=2 WHERE NAME  IN ('Pikachu','Gabumon');
+UPDATE animals SET owner_id=3 WHERE NAME IN ('Devimon','Plantmon');
+UPDATE animals SET owner_id=4 WHERE NAME IN ('Charmander','Squirtle','Blossom');
+UPDATE animals SET owner_id=5 WHERE NAME IN ('Angemon','Boarmon');
 
